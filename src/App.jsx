c25d84eeba9ea65eb098a5e97fa46e035e69ad8c
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-09 12:45:13 -0400

    Modificado arreglo por arreglo de objetos

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  const cars = ['BMW', 'Mercedes', 'Volvo']
+  const cars = [
+    {id: 1, brand: 'BMW'},
+    {id: 2, brand: 'Mercedes'},
+    {id: 3, brand: 'Volvo'}
+  ]
 
   return (
     <section className="container my-2">
       <ul>
-        {cars.map(car => <li>I am a {car}</li>)}
+        {cars.map(car => <li key={car.id}>I am a {car.brand}</li>)}
       </ul>
       
     </section>
